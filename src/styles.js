c68c8e9f5fb31c86fa4f2da47283a4bb1f6a87a3
Drenilson <andre_luiz1907@hotmail.com>
2024-10-18 03:11:29 +0000

    Alteração e finalização no App e styles

--- src/styles.js
+++ src/styles.js
@@ -3,30 +3,30 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: #CACACA;
-
+    background-color: #2E2E2E;
     display: flex;
     align-items: center;
     justify-content: center;
 `
 
-
 export const Content = styled.div`
-    background-color: #FFFFFF;
+    background-color: #FF8C00;
     width: 50%;
+    border: 2px solid #800080;
 `
 
 export const Row = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    color: #800080;
 `
 
 export const Column = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    color: #FF8C00; /* Laranja escuro */
 `
-
